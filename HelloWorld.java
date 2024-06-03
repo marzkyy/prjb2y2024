e60fb4b050cd228ac1adf8040ed291ee8daadcf1
marzkyy <junmarjm@gmail.com>
2024-06-03 04:48:50 +0000

    Errors Fixed for task sheet 1-1-1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-clas helloWorldApp {
+class helloWorldApp {
     public static void main(String[] args) {
-        System.out.rintln("Hello World!); // Display the string.
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
