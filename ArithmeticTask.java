8ce01dfbdcfc91ce3618c3bbefffcec79acd163f
marzkyy <junmarjm@gmail.com>
2024-06-05 20:03:44 +0800

    Program changed to Use compound assignments, program compiled with Java Standard specification

--- ArithmeticTask.java
+++ ArithmeticTask.java
@@ -5,17 +5,17 @@ class ArithmeticTask {
         int result = 1 + 2; // result is now 3
         System.out.println(result);
 
-        result = result - 1; // result is now 2
+        result -= 1; // result is now 2
         System.out.println(result);
 
-        result = result * 2; // result is now 4
+        result *= 2; // result is now 4
         System.out.println(result);
 
-        result = result / 2; // result is now 2
+        result /= 2; // result is now 2
         System.out.println(result);
 
-        result = result + 8; // result is now 10
-        result = result % 7; // result is now 3
+        result += 8; // result is now 10
+        result %= 7; // result is now 3
         System.out.println(result);
 
     }
